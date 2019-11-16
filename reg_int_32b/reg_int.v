4d8db898e56275e7126f0c2e9da74f7ef59d9dba
Misael Cézar Gomes de Souza <43627477+MisaelCzar@users.noreply.github.com>
2019-11-15 21:30:57 -0300

    Add files via upload

---
+++ reg_int_32b/reg_int.v
@@ -0,0 +1,21 @@
+module reg_int (
+clk,
+reset,
+data_in,
+data_out
+);
+parameter DATA_WIDTH = 32 ;
+
+input clk;
+input reset;
+input [DATA_WIDTH-1:0] data_in;
+output reg [DATA_WIDTH-1:0] data_out ;
+
+
+always @(posedge clk or posedge reset) begin
+	if (reset) data_out <= 32'b0;
+	else if (clk) begin
+		data_out <= data_in;
+	end
+end
+endmodule
