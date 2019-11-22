303cf22220854c93a8101ea4dfd5c5a08d6000e3
Matheus Candido <matheusoc@live.com>
2019-11-22 12:31:31 -0300

    AddressDecoder corrigido e TB funcionando

--- AddressDecoder/addressdecoder.v
+++ AddressDecoder/addressdecoder.v
@@ -1,22 +1,11 @@
 module addressdecoder(
-	in, out, cs
+	in, cs
 );
 
 input [31:0] in;
-output reg [31:0] out;
 output cs; // 1 para memória interna, 0 para memória externa
 
 // 1000h a 13ffh é memória interna
-// refazer expressão
-assign cs = (!in[11] && !in[10] && !in[3] && ( !in[7] || !in[6] || !in[5] || !in[4] ));
-
-// remover
-always @ (in) begin
-	if(cs) begin
-		out = in - 'h1000;
-	end else begin
-		out = in;
-	end
-end
+assign cs = !in[15] && !in[14] && !in[13] && in[12] && !in[11] && !in[10];
 
 endmodule
--- AddressDecoder/addressdecoder_TB.v
+++ AddressDecoder/addressdecoder_TB.v
@@ -3,22 +3,20 @@ module addressdecoder_TB();
 
 	reg [31:0] in ;
 	wire cs;
-	wire [31:0] out;
 	
 	addressdecoder DUT(
 		.in(in),
-		.out(out),
 		.cs(cs)
 	);
 	
 	initial begin
 	
-		in = 'h380;
-		#100 in = 32'h1000;
-		#100 in = 32'h1002;
-		#100 in = 32'h2000;
-		#100 in = 32'h2fff;
-		#800 $stop;
+		in = 32'h1400;
+		#100 in = 32'h13ff;
+		#200 in = 32'h1000;
+		#300 in = 32'h1001;
+		#400 in = 32'h2000;
+		#2000 $stop;
 			
 	end
 	
