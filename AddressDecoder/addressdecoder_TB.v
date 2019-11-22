f48542271efa3762d8c9e4aaf7dfc8d6f3ab377e
Matheus Candido <matheusoc@live.com>
2019-11-21 21:47:43 -0300

    Testbench do addressdecoder

--- AddressDecoder/addressdecoder_TB.v
+++ AddressDecoder/addressdecoder_TB.v
@@ -1,3 +1,26 @@
+`timescale 1ns/10ps
 module addressdecoder_TB();
 
+	reg [31:0] in ;
+	wire cs;
+	wire [31:0] out;
+	
+	addressdecoder DUT(
+		.in(in),
+		.out(out),
+		.cs(cs)
+	);
+	
+	initial begin
+	
+		in = 'h380;
+		#100 in = 32'h1000;
+		#100 in = 32'h1002;
+		#100 in = 32'h2000;
+		#100 in = 32'h2fff;
+		#800 $stop;
+			
+	end
+	
+	
 endmodule
